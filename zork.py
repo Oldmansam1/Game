92a1495a79671cc34391b4762fd401e4c05c8f5e
Sam Delamere <sam.delamere@gmail.com>
2014-12-13 16:18:59 -0900

    Your mom

--- zork.py
+++ zork.py
@@ -4,8 +4,8 @@ class luke1:
     self.legs = legs
     self.color = color
         
-luke = luke1('human', 2, 'white')
-sam = luke1('human', 2, 'Perfectly tanned')
+luke = luke1('Doctor who fanatic', 2, 'white')
+sam = luke1('Wizzard', 2, 'Perfectly tanned')
 print('Lets learn about two muggles named Luke and Sam')
 print('Say \'Ok\' if you would like to continue')
 learn = input('Type here: ')
@@ -19,22 +19,22 @@ info_input = [sam.species, sam.legs, sam.color, luke.species, luke.legs, luke.co
 input_1 = input('Type \'species\', \'legs\', or \'color\':')
 if input_1 == 'species':
   print('do you want to know about Luke or Sam?')
-  sam_1 = input('Who would you like to learn about: ')
-  if input == 'Sam':
+  input_1 = input('Type \'Luke\' or \'Sam\' to become an educated being: ')
+  if input_1 == 'Sam':
     print(sam.species)
-  if input == 'Luke':
+  if input_1 == 'Luke':
     print(luke.species)
 elif input_1 == 'legs':
   print('do you want to know about luke or Sam?')
-  if input == 'Sam':
+  if input_1 == 'Sam':
     print(sam.legs)
-  if input == 'Luke':
+  if input_1 == 'Luke':
     print(luke.legs)
 elif input_1 == 'color':
   print('do you want to know about luke or Sam?')
-  if input == 'Sam':
+  if input_1 == 'Sam':
     print(sam.color)
-  if input == 'Luke':
+  if input_1 == 'Luke':
     print(luke.color)
   
 
